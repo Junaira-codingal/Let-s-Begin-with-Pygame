64fa8c91e2b9ad4e1f91d6088f60e74215111830
Junaira <advshakerullah@gmail.com>
2024-11-01 11:49:14 +0600

    first commit

---
+++ Window .py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((400,300))
+color = (247 , 79 , 100)
+
+screen.fill(color)
+
+done = False
+while not done:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        
+    pygame.display.flip()
